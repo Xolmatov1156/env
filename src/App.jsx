2fa36b00bd8b7cb0c661fa91098ca6b6b28c179b
Xolmatov1156 <xolmatov1156@gmail.com>
2024-08-28 01:31:57 +0500

    env

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Shop from "./pages/Shop";
+import React, { } from "react";
 import "./App.css";
-import { ThreeDots } from 'react-loader-spinner';
 import CustomRoutes from "./routes";
 
 function App() {
